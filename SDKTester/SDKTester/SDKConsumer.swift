37cdd8d354a744bc26ff54951a51770ceb4c7fec
William Prevett <will@sparklit.com>
2020-10-15 12:35:50 -0700

    Add frequency capping, remove UIWebView

--- SDKTester/SDKTester/SDKConsumer.swift
+++ SDKTester/SDKTester/SDKConsumer.swift
@@ -120,7 +120,7 @@ class SDKConsumer : NSObject {
                     self.log("Error - Invalid placement")
                     return
                 }
-                if(placements[0].body != nil && placements[0].body != ""){
+                if((placements[0].body != nil && placements[0].body != "") || placements[0].imageUrl != nil){
                     self.interstitial = PWInterstitial(placement:placements[0], parentViewController:self.parentViewController, delegate:self.interstitialDelegate!, respectSafeAreaLayoutGuide:true)
                 }
             default:
